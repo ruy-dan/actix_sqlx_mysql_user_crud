b98a11119db481f253eacd4a5760f2f98bf45e78
ruy-dan <ruy-dan@protonmail.com>
2023-09-23 18:45:11 +0200

    adapt config

--- schema.sql
+++ schema.sql
@@ -5,21 +5,21 @@ DROP TABLE IF EXISTS `users_to_groups`;
 DROP TABLE IF EXISTS `groups`;
 DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE IF NOT EXISTS users 
+CREATE TABLE IF NOT EXISTS users
 (
 	id VARCHAR(48) NOT NULL UNIQUE,
 	name VARCHAR(64) NOT NULL UNIQUE,
 	email VARCHAR(256) NOT NULL UNIQUE,
 	PRIMARY KEY (id)
 );
-            
+
 CREATE TABLE IF NOT EXISTS `groups`
 (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(64) NOT NULL UNIQUE,
     PRIMARY KEY(id)
 );
-            
+
 CREATE TABLE IF NOT EXISTS `users_to_groups`
 (
     `user_id` VARCHAR(48) NOT NULL,
@@ -28,5 +28,5 @@ CREATE TABLE IF NOT EXISTS `users_to_groups`
     FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`)
 );
 
-CREATE USER IF NOT EXISTS 'sqlx_user_crud'@'localhost' IDENTIFIED BY 'rust_is_the_future';
+CREATE USER IF NOT EXISTS 'sqlx_user_crud'@'127.0.0.1' IDENTIFIED BY 'rust_is_the_future';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `actix_user_crud`.* TO 'sqlx_user_crud'@'localhost';
